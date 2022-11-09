e311a0f1c48ef93593ff3aa352d664542117b8d4
ds797 <73960745+ds797@users.noreply.github.com>
2022-11-08 17:37:43 -0800

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -1,68 +1,19 @@
 <script>
-	import web from './assets/web.svg';
-	import js from './assets/js.svg';
-	import svelte from './assets/svelte.svg';
-	import html from './assets/html.svg';
-	import css from './assets/css.svg';
-	import c from './assets/c.svg';
-	import cpp from './assets/c++.svg';
-	import nasm from './assets/nasm.svg';
-	import Language from './Language.svelte';
-	import Github from './Github.svelte';
+	import Navigation from './Navigation.svelte';
+	import About from './pages/About.svelte';
+	import Skills from './pages/Skills.svelte';
+	import Projects from './pages/Projects.svelte';
 
-	let languages = [{
-		name: 'Web',
-		icon: web,
-		proficiency: 5,
-		children: [{
-			name: 'HTML',
-			icon: html,
-			proficiency: 5
-		}, {
-			name: 'CSS',
-			icon: css,
-			proficiency: 5
-		}, {
-			name: 'JavaScript',
-			icon: js,
-			proficiency: 5
-		}, {
-			name: 'Svelte',
-			icon: svelte,
-			proficiency: 5
-		}]
-	}, {
-		name: 'C++',
-		icon: cpp,
-		proficiency: 4
-	}, {
-		name: 'C',
-		icon: c,
-		proficiency: 4
-	}, {
-		name: 'Assembly (NASM)',
-		icon: nasm,
-		proficiency: 4
-	}];
+	let page = 0;
 </script>
 
 <main>
-	<h1>Hello! I'm ds797.</h1>
-	<Github />
-	{ #each languages as language (language) }
-		<Language {language} />
-	{ /each }
+	<Navigation bind:page />
+	<About />
+	<Skills />
+	<Projects />
 </main>
 
 <style>
-	main {
-		padding: 2rem 8rem;
-		display: flex;
-		flex-flow: column;
-		justify-content: center;
-	}
 
-	h1 {
-		font-size: 4rem;
-	}
 </style>
---
+++ src/pages/About.svelte
@@ -0,0 +1,20 @@
+<main>
+	<h2>I'm ds797, a student, programmer, and designer.</h2>
+	<p>I love deep-diving into subjects I don't know much about, then coming out the other side brimming with knowledge. The universe, nature, and technology are some of my favorite subjects!</p>
+	<p>I'm very passionate about programming, I understand how it works and I see limitless potential. I enjoy writing terse but readable code that performs its job well.</p>
+	<p>I love designing user interfaces and implementing them through my programming skills. It's a great way for me to challenge myself, and I enjoy the journey of creating an experience as much as the result.</p>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-flow: column;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	p {
+		max-width: 35vw;
+	}
+</style>
