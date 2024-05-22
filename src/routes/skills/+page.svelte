844ed37e5d3b77fea6925632b09191b8e699883c
ds797 <ds797@duck.com>
2024-05-22 13:13:22 -0700

    Chore: Remove unused variables

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import web from '$lib/assets/web.svg';
 	import js from '$lib/assets/js.svg';
 	import react from '$lib/assets/react.svg';
 	import svelte from '$lib/assets/svelte.svg';
@@ -9,50 +8,6 @@
 	import cpp from '$lib/assets/c++.svg';
 	import nasm from '$lib/assets/nasm.svg';
 	import python from '$lib/assets/python.svg';
-	import Language from '$lib/components/Language.svelte';
-
-	let languages = [{
-		name: 'Web',
-		icon: web,
-		proficiency: 5,
-		children: [{
-			name: 'HTML',
-			icon: html,
-			proficiency: 5
-		}, {
-			name: 'CSS',
-			icon: css,
-			proficiency: 5
-		}, {
-			name: 'JavaScript',
-			icon: js,
-			proficiency: 5
-		}, {
-			name: 'React',
-			icon: react,
-			proficiency: 5
-		}, {
-			name: 'Svelte',
-			icon: svelte,
-			proficiency: 4
-		}]
-	}, {
-		name: 'C++',
-		icon: cpp,
-		proficiency: 4
-	}, {
-		name: 'C',
-		icon: c,
-		proficiency: 4
-	}, {
-		name: 'Assembly (NASM)',
-		icon: nasm,
-		proficiency: 4
-	}, {
-		name: 'Python',
-		icon: python,
-		proficiency: 3
-	}];
 </script>
 
 <main>
